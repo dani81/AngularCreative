9f332b23aeda043136908e5801a70e53e9bfb94b
dani81 <31867232+dani81@users.noreply.github.com>
2017-10-18 14:27:17 -0600

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,10 +1,15 @@
 angular.module('pokemonBattle', [])
 .controller('MainCtrl', [
   '$scope',
+  $scope.posts = {};
   function($scope)
   {
     $scope.addPokemon = function(){
-      
+      if($scope.formContent === '') { return; } 
+      $scope.posts.push({ 
+        team: $scope.team, pokemon: $scope.pokemon, position: $scope.position 
+      }); 
+      $scope.formContent = '';
     }
   }
   ]);
