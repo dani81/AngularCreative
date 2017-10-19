ad6abb7b385667c3964bcd00ffb42acff949a197
dani81 <31867232+dani81@users.noreply.github.com>
2017-10-19 14:06:19 -0600

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -3,16 +3,28 @@ angular.module('pokemonBattle', [])
   .controller('MainCtrl', [
     '$scope',
     function($scope){
-     $scope.posts = [];
+     $scope.teamOne = [];
+     $scope.teamTwo = [];
       $scope.addPokemon = function(){
         console.log($scope.pokemon);
-          $scope.posts.push({
+       if($scope.team = "Team 1"){
+          $scope.teamOne.push({
           title: $scope.team,
           pokemonName: $scope.pokemon,
           picture: $scope.pokemon + ".png"
         });
        console.log("here3");
-       console.log($scope.posts[0]);
+       console.log($scope.teamOne[0]);
+       }
+       else{
+        $scope.teamTwo.push({
+          title: $scope.team,
+          pokemonName: $scope.pokemon,
+          picture: $scope.pokemon + ".png"
+        });
+       console.log("here3");
+       console.log($scope.teamTwo[0]);
+       }
       };
   }
 ]);
